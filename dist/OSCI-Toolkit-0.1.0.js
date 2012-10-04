be7fa928878a64ee307f7996dfee318f9da26646
Kyle Jaebker <kjaebker@gmail.com>
2012-10-04 11:23:08 -0400

    fix build error

--- dist/OSCI-Toolkit-0.1.0.js
+++ dist/OSCI-Toolkit-0.1.0.js
@@ -3467,40 +3467,6 @@ OsciTk.views.Toolbar = OsciTk.views.BaseView.extend({
 		this.isContentOpen = false;
 	}
 });
-app.zotero = {
-    init: function() {
-        app.dispatcher.on('packageLoaded', function(model) {
-            // Get date
-            var d = new Date(model.get('dc:date'));
-            d = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
-
-            // Build COInS data
-            var coins = [
-                'ctx_ver=Z39.88-2004',
-                'rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook',
-                'rft.genre=book',
-                'rft.date=' + d,
-                'rfr_id=' + model.get('dc:identifier'),
-                'rft.btitle=' + model.get('dc:title'),
-                'rft.atitle=' + model.get('dc:title'),
-                'rft.au=' + model.get('dc:creator'),
-                'rft.pub=' + model.get('dc:publisher')
-            ];
-
-            // Append coins data to body
-            var span = $('<span></span>');
-            span.addClass('Z3988');
-            span.attr('title', coins.join('&'));
-            span.appendTo($('body'));
-        
-            // Trigger zotero to search for biblio data
-            var ev = document.createEvent('HTMLEvents');
-            ev.initEvent('ZoteroItemUpdated', true, true);
-            document.dispatchEvent(ev);
-        });
-    }
-};
-
 app = {
 	dispatcher : undefined,
 	router : undefined,
@@ -3541,4 +3507,38 @@ app = {
 		this.models.docPackage = new OsciTk.models.Package({url: this.config.get('packageUrl')});
 		Backbone.history.start();
 	}
+};
+
+app.zotero = {
+    init: function() {
+        app.dispatcher.on('packageLoaded', function(model) {
+            // Get date
+            var d = new Date(model.get('dc:date'));
+            d = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+
+            // Build COInS data
+            var coins = [
+                'ctx_ver=Z39.88-2004',
+                'rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook',
+                'rft.genre=book',
+                'rft.date=' + d,
+                'rfr_id=' + model.get('dc:identifier'),
+                'rft.btitle=' + model.get('dc:title'),
+                'rft.atitle=' + model.get('dc:title'),
+                'rft.au=' + model.get('dc:creator'),
+                'rft.pub=' + model.get('dc:publisher')
+            ];
+
+            // Append coins data to body
+            var span = $('<span></span>');
+            span.addClass('Z3988');
+            span.attr('title', coins.join('&'));
+            span.appendTo($('body'));
+        
+            // Trigger zotero to search for biblio data
+            var ev = document.createEvent('HTMLEvents');
+            ev.initEvent('ZoteroItemUpdated', true, true);
+            document.dispatchEvent(ev);
+        });
+    }
 };
--- grunt.js
+++ grunt.js
@@ -35,9 +35,9 @@ module.exports = function(grunt) {
 					'js/oscitk/views/SectionView.js',
 					'js/oscitk/views/MultiColumnFigureView.js',
 					'js/oscitk/views/**/*.js',
-					'js/oscitk/zotero.js',
 					'js/oscitk/osci_tk_layerd_image.js',
-					'js/appBootstrap.js'
+					'js/appBootstrap.js',
+					'js/oscitk/zotero.js'
 				],
 				dest: 'dist/OSCI-Toolkit-<%= meta.version %>.js'
 			},
