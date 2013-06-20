8a98d43f6d86ec5d2df6608aba9534eaf69868d0
Kris Arnold <karnold@dma.org>
2013-06-20 16:03:01 -0500

    removed minified files

--- dist/js/OSCI-Toolkit-0.1.0.js
+++ dist/js/OSCI-Toolkit-0.1.0.js
@@ -870,7 +870,12 @@ OsciTk.models.Package = OsciTk.models.BaseModel.extend({
 		}
 
 		return title;
-	}
+	},
+    getPubId: function() {
+        var metadata = this.get("metadata");
+        var vals = metadata['dc:identifier'].value.split(":");
+        return vals[2];
+    }
 });
 
 OsciTk.models.Page = OsciTk.models.BaseModel.extend({
