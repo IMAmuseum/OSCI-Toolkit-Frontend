607b5dd4f6b6a5838f3e19c49589c2fc31a2ead8
Kyle Jaebker <kjaebker@gmail.com>
2013-06-21 13:58:00 -0400

    updated to account for multiple identifiers

--- js/oscitk/models/PackageModel.js
+++ js/oscitk/models/PackageModel.js
@@ -1,61 +1,74 @@
 OsciTk.models.Package = OsciTk.models.BaseModel.extend({
-	defaults: function() {
-		return {
-			url: null,
-			lang: null,
-			spine: null,
-			manifest: null,
-			metadata: null,
-			id: null,
-			version: null,
-			xmlns: null
-		};
-	},
+    defaults: function() {
+        return {
+            url: null,
+            lang: null,
+            spine: null,
+            manifest: null,
+            metadata: null,
+            id: null,
+            version: null,
+            xmlns: null
+        };
+    },
 
-	initialize: function() {
-		// TODO: ERROR CHECK THE RETURN XML
-		var data = xmlToJson(loadXMLDoc(this.get('url')));
+    initialize: function() {
+        // TODO: ERROR CHECK THE RETURN XML
+        var data = xmlToJson(loadXMLDoc(this.get('url')));
 
-		this.set('lang', data['package'].lang);
-		this.set('spine', data['package'].spine);
-		this.set('manifest', data['package'].manifest);
-		this.set('metadata', data['package'].metadata);
+        this.set('lang', data['package'].lang);
+        this.set('spine', data['package'].spine);
+        this.set('manifest', data['package'].manifest);
+        this.set('metadata', data['package'].metadata);
 
-		//get the publication id
-		var ids = data['package']['metadata']['dc:identifier'];
-		if (!_.isArray(ids)) {
-			ids = [ids];
-		}
-		var numIds = ids.length;
-		for (var i = 0; i < numIds; i++) {
-			var pubId = ids[i];
-			if (pubId.value.indexOf('urn:osci_tk_identifier:') !== false) {
-				this.set('id', pubId.value.substr(23));
-				break;
-			}
-		}
+        //get the publication id
+        var ids = data['package']['metadata']['dc:identifier'];
+        if (!_.isArray(ids)) {
+            ids = [ids];
+        }
+        var numIds = ids.length;
+        for (var i = 0; i < numIds; i++) {
+            var pubId = ids[i];
+            if (pubId.value.indexOf('urn:osci_tk_identifier:') !== false) {
+                this.set('id', pubId.value.substr(23));
+                break;
+            }
+        }
 
-		this.set('version', data['package'].version);
-		this.set('xmlns', data['package'].xmlns);
+        this.set('version', data['package'].version);
+        this.set('xmlns', data['package'].xmlns);
 
-		Backbone.trigger('packageLoaded', this);
-	},
+        Backbone.trigger('packageLoaded', this);
+    },
 
-	sync: function(method, model, options) {
-	},
+    sync: function(method, model, options) {
+    },
 
-	getTitle: function() {
-		var title;
-		var metadata = this.get("metadata");
-		if (metadata['dc:title'] && metadata['dc:title']['value']) {
-			title = metadata['dc:title']['value'];
-		}
+    getTitle: function() {
+        var title;
+        var metadata = this.get("metadata");
+        if (metadata['dc:title'] && metadata['dc:title']['value']) {
+            title = metadata['dc:title']['value'];
+        }
 
-		return title;
-	},
+        return title;
+    },
     getPubId: function() {
         var metadata = this.get("metadata");
-        var vals = metadata['dc:identifier'].value.split(":");
+        var vals;
+
+        if (!_.isUndefined(metadata['dc:identifier'])) {
+            if (_.isArray(metadata['dc:identifier'])) {
+                var pubId = _.find(metadata['dc:identifier'], function(ident) {
+                    return ident.id === "publication-id" ? true : false;
+                });
+                if (pubId) {
+                    vals = pubId.value.split(":");
+                }
+            } else {
+                vals = metadata['dc:identifier'].value.split(":");
+            }
+        }
         return vals[2];
     }
 });
