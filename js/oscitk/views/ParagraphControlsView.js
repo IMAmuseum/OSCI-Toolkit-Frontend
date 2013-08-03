59974f995765e80c1bde92ced83ad7c2ce58ed3c
Kyle Jaebker <kjaebker@gmail.com>
2013-08-03 09:55:06 -0400

    dropping event for qtip config

--- js/oscitk/views/ParagraphControlsView.js
+++ js/oscitk/views/ParagraphControlsView.js
@@ -1,79 +1,80 @@
 OsciTk.views.ParagraphControlsView = OsciTk.views.BaseView.extend({
-	className: 'paragraph-controls',
+    className: 'paragraph-controls',
 
-	initialize: function(options) {
-		this.options = options;
-		this.options.paragraphNumber = this.options.content.data("paragraph_number");
-		this.options.contentIdentifier = this.options.content.data("osci_content_id");
-		this.options.linkItems = app.config.get('paragraphControls');
+    initialize: function(options) {
+        this.options = options;
+        this.options.paragraphNumber = this.options.content.data("paragraph_number");
+        this.options.contentIdentifier = this.options.content.data("osci_content_id");
+        this.options.linkItems = app.config.get('paragraphControls');
 
-		if (this.options.linkItems) {
-			this.render();
+        if (this.options.linkItems) {
+            this.render();
 
-			this.listenTo(Backbone, "paragraphClicked", function(data) {
-				var pNum = data.paragraphNumber;
-				if (this.options.paragraphNumber === pNum) {
-					var tip = this.$el.qtip("api");
-					tip.show();
-				}
-			});
-		}
-	},
+            // this.listenTo(Backbone, "paragraphClicked", function(data) {
+            //  var pNum = data.paragraphNumber;
+            //  if (this.options.paragraphNumber === pNum) {
+            //      var tip = this.$el.qtip("api");
+            //      tip.show();
+            //  }
+            // });
+        }
+    },
 
-	render: function() {
-		var contentPosition = this.options.content.position();
+    render: function() {
+        var contentPosition = this.options.content.position();
 
-		this.$el.attr('data-osci_content_id', this.options.contentIdentifier);
-		this.$el.attr('data-paragraph_identifier', this.options.paragraphNumber);
-		this.$el.html('<span class="paragraph-identifier paragraph-identifier-' + this.options.paragraphNumber + '">' + (this.options.paragraphNumber) + '</span>');
-		this.$el.css(this.options.position);
+        this.$el.attr('data-osci_content_id', this.options.contentIdentifier);
+        this.$el.attr('data-paragraph_identifier', this.options.paragraphNumber);
+        this.$el.html('<span class="paragraph-identifier paragraph-identifier-' + this.options.paragraphNumber + '">' + (this.options.paragraphNumber) + '</span>');
+        this.$el.css(this.options.position);
 
-		//remove qtip if already present
-		if(this.$el.data("qtip")) {
-			this.$el.qtip("destroy", "true");
-		}
+        //remove qtip if already present
+        if(this.$el.data("qtip")) {
+            this.$el.qtip("destroy", "true");
+        }
 
-		var tipContent = '';
-		for(var i in this.options.linkItems) {
-			var text = this.options.linkItems[i];
-			tipContent += '<a href="' + i + '" data-event="' + i + '" class="' + i +'">' + text + '</a> ';
-		}
+        var tipContent = '';
+        for(var i in this.options.linkItems) {
+            var text = this.options.linkItems[i];
+            tipContent += '<a href="' + i + '" data-event="' + i + '" class="' + i +'">' + text + '</a> ';
+        }
 
-		this.$el.qtip({
-			position: {
-				my: "left center",
-				at: "right center",
-				target: this.$el,
-				container: this.$el,
-				adjust: {
-					y: -10
-				}
-			},
-			show: {
-				ready: false,
-				solo: true
-			},
-			hide: {
-				fixed: true,
-				delay: 500
-			},
-			style: {
-				def: false
-			},
-			overwrite: false,
-			content: tipContent
-		});
+        this.$el.qtip({
+            position: {
+                my: "left center",
+                at: "right center",
+                target: this.$el,
+                container: this.$el,
+                adjust: {
+                    y: -10
+                }
+            },
+            show: {
+                target: $("p[data-osci_content_id='" + this.options.contentIdentifier + "']"),
+                ready: false,
+                solo: true
+            },
+            hide: {
+                fixed: true,
+                delay: 500
+            },
+            style: {
+                def: false
+            },
+            overwrite: false,
+            content: tipContent
+        });
 
-		this.$el.on('click', 'a', {content: this.options.content}, function(e) {
-			e.preventDefault();
-			Backbone.trigger(
-				$(this).data('event'),
-				{
-					contentId: $(e.data.content).attr('data-osci_content_id')
-				}
-			);
-		});
+        this.$el.on('click', 'a', {content: this.options.content}, function(e) {
+            e.preventDefault();
+            Backbone.trigger(
+                $(this).data('event'),
+                {
+                    contentId: $(e.data.content).attr('data-osci_content_id')
+                }
+            );
+        });
 
-		return this;
-	}
+        return this;
+    }
 });
