046156b49ab868248e3fa3d169cccee674f19e19
Kyle Jaebker <kjaebker@gmail.com>
2014-01-21 14:37:38 -0500

    only allow tooltip no click on glossary terms

--- js/oscitk/views/GlossaryTooltipView.js
+++ js/oscitk/views/GlossaryTooltipView.js
@@ -1,35 +1,37 @@
 OsciTk.views.GlossaryTooltip = OsciTk.views.BaseView.extend({
-	initialize: function() {
-		this.listenTo(Backbone, 'layoutComplete', function() {
-			if (app.collections.glossaryTerms.length !== 0) {
-				$('.glossary-term').qtip({
-					content: {
-						title: ' ',
-						text: ' '
-					},
-					position: {
-						viewport: $(window)
-					},
-					style: {
-						classes: 'glossary-tooltip',
-						def: false,
-						width: app.views.sectionView.dimensions.columnWidth + 'px'
-					},
-					events: {
-						show: function(event, api) {
-							var tid = $(event.originalEvent.target).data('tid');
-							var item = app.collections.glossaryTerms.get(tid);
-							// set the tooltip contents
-							api.set('content.title', item.get('term'));
-							api.set('content.text', item.get('definition'));
-						}
-					}
-				});
-			}
-		});
+    initialize: function() {
+        this.listenTo(Backbone, 'layoutComplete', function() {
+            if (app.collections.glossaryTerms.length !== 0) {
+                $('.glossary-term').qtip({
+                    content: {
+                        title: ' ',
+                        text: ' '
+                    },
+                    position: {
+                        viewport: $(window)
+                    },
+                    style: {
+                        classes: 'glossary-tooltip',
+                        def: false,
+                        width: app.views.sectionView.dimensions.columnWidth + 'px'
+                    },
+                    events: {
+                        show: function(event, api) {
+                            var tid = $(event.originalEvent.target).data('tid');
+                            var item = app.collections.glossaryTerms.get(tid);
+                            // set the tooltip contents
+                            api.set('content.title', item.get('term'));
+                            api.set('content.text', item.get('definition'));
+                        }
+                    }
+                }).click(function(e) {
+                    e.preventDefault();
+                });
+            }
+        });
 
-		this.listenTo(Backbone, 'routedToSection', function(section) {
-			$('.glossary-tooltip').qtip('destroy');
-		});
-	}
+        this.listenTo(Backbone, 'routedToSection', function(section) {
+            $('.glossary-tooltip').qtip('destroy');
+        });
+    }
 });
