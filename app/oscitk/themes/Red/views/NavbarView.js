8cecb4be81b43cf63ead48c7f8d3102fb5514027
Illya Moskvin <imoskvin@imamuseum.org>
2015-12-10 12:23:53 -0500

    Removed _layered-image.scss and _progress.scss

--- app/oscitk/themes/Red/views/NavbarView.js
+++ app/oscitk/themes/Red/views/NavbarView.js
@@ -6,38 +6,9 @@ OsciTk.views.Navbar = OsciTk.views.BaseView.extend({
 	},
 	initialize: function() {
 		
-		
-
-		this.listenTo(Backbone, 'packageLoaded', function(packageModel) {
-
-			// Get total number of pages
-			// Get current page
-			// Instantiate progress bar
-			// Draw the toggle single / double
-
-			//this.creator = $(packageModel)[0].attributes['metadata']['dc:creator'];
-			//this.pubTitle = packageModel.getTitle();
-			//this.sections = app.collections.navigationItems.where({depth: 0});
-
-
-
-			//console.log( this.sections );
-
-		});
-
 		this.listenTo(Backbone, "currentNavigationItemChanged", function() {
 			this.render();
 		});
-
-		/*
-		this.listenTo(Backbone, "sectionLoaded", function() {
-			this.sliderResize();
-		});
-
-		// We must wait for the images and fonts to download to trigger this event
-		$(window).load( this.sliderResize );
-		$(window).resize( this.sliderResize );
-		*/
 
 	},
 
@@ -48,7 +19,7 @@ OsciTk.views.Navbar = OsciTk.views.BaseView.extend({
 		$('.navbar-item[data-toggle="tooltip"]').tooltip({left:'150px'});
 
 		// Render the font size selector
-		var item = new OsciTk.views.ToolbarItem({toolbarItem: {view: 'fontSizeView', text: 'font-size', style: 'inline'} });
+		var item = new OsciTk.views.ToolbarItem( { toolbarItem: { view: 'fontSizeView', text: 'font-size', style: 'inline' } } );
 		this.addView(item, '#font-size-area');
 		item.render();
 
@@ -70,29 +41,4 @@ OsciTk.views.Navbar = OsciTk.views.BaseView.extend({
 
 	},
 
-	/*
-	sliderResize: function() {
-
-		
-		var $slider = $('#osci-page-slider');
-		var width   = $slider.parent().width();
-			width  -= $('#osci-navbar-text').outerWidth() + 1;
-			width  -= $('#osci-spread-selector').outerWidth() + 1;
-			width  -= $('#font-size-area').outerWidth() + 1;
-
-		$slider.innerWidth( width );
-
-		
-		console.log( 
-			$('#osci-page-slider').parent().width(), 
-			$('#osci-navbar-text').outerWidth(),
-			$('#font-size-area').outerWidth(),
-			$('#osci-spread-selector').outerWidth()
-		);
-		
-
-		
-
-	}
-	*/
 });
